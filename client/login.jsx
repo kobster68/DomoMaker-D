c9bc9da7b20d2eeafb792a6c8f3a847a13c9f8c5
kobster68 <kobe.laprade@gmail.com>
2024-04-07 15:52:04 -0400

    possible syntax fix for heroku

--- client/login.jsx
+++ client/login.jsx
@@ -1,6 +1,6 @@
 const helper = require('./helper.js');
 const React = require('react');
-const {createRoot} = require('react-dom/client');
+const { createRoot } = require('react-dom/client');
 
 const handleLogin = (e) => {
     e.preventDefault();
